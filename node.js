28100bff524bccedb2d6307b5b57afbcfd6d25ae
justayak <justayak@googlemail.com>
2014-02-28 17:48:51 +0100

    impls ...

--- node.js
+++ node.js
@@ -1,3 +1,23 @@
 var OG = require("./orthoGrid");
 
-var Grid = new OG.Grid(5,8);
+var Grid = new OG.Grid(10,8);
+
+var a = Grid.spawnEntity({
+    w : 2,
+    h : 2,
+    x : 3,
+    y : 2
+});
+
+var b = Grid.spawnEntity({
+    w : 1,
+    h : 1,
+    x : 1,
+    y : 1
+});
+
+var dir = OG.Directions;
+
+b.move(dir.n);
+
+Grid.print();
--- orthoGrid.js
+++ orthoGrid.js
@@ -1,8 +1,9 @@
-(function(){
+(function () {
 
     "use strict";
 
-    var OG = function(){};
+    var OG = function () {
+    };
 
     var INT_BYTE_SIZE = 2;
 
@@ -17,12 +18,14 @@
      *
      * @type {Function}
      */
-    var Grid = OG.Grid = function(width,height){
+    var Grid = OG.Grid = function (width, height) {
         this.h = height;
         this.w = width;
         var buffer = new ArrayBuffer(height * width);
         var userBuffer = new ArrayBuffer(height * width * INT_BYTE_SIZE);
-        this.entities = [{name:"dummy"}];
+        this.entities = [
+            {name:"dummy"}
+        ];
 
         /**
          * static values
@@ -47,16 +50,42 @@
         this.trapLookup = {};
     };
 
-    Grid.prototype.spawnEntity = function(entityOptions){
+    /**
+     *
+     * @param entityOptions {Option}
+     *      w : {Integer} Defines, how many grids this element will consume
+     *      h : {Integer} Defines, how many grids this element will consume
+     *      x : {Integer} position on the grid
+     *      Y : {Integer} position on the grid
+     *      group : {Integer} {OPTIONAL}
+     * @return {*}
+     */
+    Grid.prototype.spawnEntity = function (entityOptions) {
         var pos = entityNumber;
         entityNumber += 1;
-        var e = new GridEntity(entityOptions,pos,this);
+        var e = new GridEntity(entityOptions, pos, this);
         this.entities[pos] = e;
         return e;
     };
 
-    Grid.prototype.getEntityAt = function(x,y){
+    Grid.prototype.getEntityAt = function (x, y) {
+        var i = getValueFromMatrix(this.userData, x, y, this.w);
+        if (i === 0) return null;
+        return this.entities[i];
+    };
 
+    Grid.prototype.print = function () {
+        var str = "";
+        for (var y = 0; y < this.h; y++) {
+            for (var x = 0; x < this.w; x++) {
+                var v = getValueFromMatrix(this.userData, x, y, this.w);
+                if (v === 0) str += "_"
+                else str += v;
+                str += "|";
+            }
+            str += "\n";
+        }
+        console.log(str);
     };
 
     /**
@@ -65,11 +94,12 @@
      *      h : {Integer} Defines, how many grids this element will consume
      *      x : {Integer} position on the grid
      *      Y : {Integer} position on the grid
+     *      enterableEventIds : [{Integer}]
      *      group : {Integer} {OPTIONAL}
      * }
      * @type {Function}
      */
-    var GridEntity = OG.GridEntity = function(options, id, grid){
+    var GridEntity = OG.GridEntity = function (options, id, grid) {
         if (typeof options === 'undefined') options = {};
         this.group = options.group || 1;
         this.w = options.w || 1;
@@ -77,65 +107,203 @@
         this.x = options.x || 0;
         this.y = options.y || 0;
         this.id = id;
+        this.enterableEventIds = options.enterableEventIds || [];
         this.grid = grid;
+        this.eventListener = [];
 
         // set initial position:
-        if(this.w === 1 && this.h === 1){
-            setValueToMatrix(this.grid.userData, this.x,this.y, this.grid.w, this.id);
-        }else{
+        if (this.w === 1 && this.h === 1) {
+            setValueToMatrix(this.grid.userData, this.x, this.y, this.grid.w, this.id);
+        } else {
             var w = this.grid.w;
             var X = this.x + this.w;
             var Y = this.y + this.h;
-            for(var x = this.x; x < X; x++){
-                for(var y = this.y; y < Y; y++){
-                    setValueToMatrix(this.grid.userData, x,y, w, this.id);
+            for (var x = this.x; x < X; x++) {
+                for (var y = this.y; y < Y; y++) {
+                    setValueToMatrix(this.grid.userData, x, y, w, this.id);
                 }
             }
         }
-
     };
 
-    GridEntity.prototype.forcePosition = function(x,y){
-        if(this.w === 1 && this.h === 1){
-            setValueToMatrix(this.grid.userData, this.x,this.y, this.grid.w, 0);
-            setValueToMatrix(this.grid.userData, this.x,this.y, this.grid.w, this.id);
-        }else{
+    /**
+     * @param id
+     */
+    GridEntity.prototype.validateEventId = function (id) {
+        if (id === 0) return true;
+        if (id === 1) return false;
+        if (this.enterableEventIds.length === 0) return false;
+        for (var i = 0; i < this.enterableEventIds.length; i++) {
+            if (this.enterableEventIds[i] === id) return true;
+        }
+        return false;
+    };
+
+    var Directions = OG.Directions = {
+        n:0,
+        s:1,
+        w:2,
+        e:3,
+        ne:4,
+        nw:5,
+        se:6,
+        sw:7
+    };
+
+    /**
+     *
+     * @param callback {function} with eventId, payload
+     */
+    GridEntity.prototype.onEvent = function (callback) {
+        this.eventListener.push(callback);
+    };
+
+    /**
+     *
+     * @param dir
+     */
+    GridEntity.prototype.move = function (dir) {
+        var x = this.x;
+        var y = this.y;
+        switch (dir) {
+            case Directions.n:
+                y -= 1;
+                break;
+            case Directions.s:
+                y += 1;
+                break;
+            case Directions.w:
+                x -= 1;
+                break;
+            case Directions.e:
+                x += 1;
+                break;
+            case Directions.ne:
+                x -= 1;
+                y += 1;
+                break;
+            case Directions.nw:
+                x -= 1;
+                y -= 1;
+                break;
+            case Directions.se:
+                x += 1;
+                y += 1;
+                break;
+            case Directions.sw:
+                x += 1;
+                y -= 1;
+                break;
+        }
+        x = Math.max(0, Math.min(x, this.grid.w - 1));
+        y = Math.max(0, Math.min(y, this.grid.h - 1));
+
+        var userData = this.grid.userData;
+        var data = this.grid.data;
+        var w = this.grid.w;
+        if (x !== this.x || y !== this.y) {
+            if (this.w === 1 && this.h === 1) {
+                // SINGLE ELEMENT
+                var person = getValueFromMatrix(userData, x, y, w);
+                if (person === 0) {
+                    var event = getValueFromMatrix(data, x, y, w);
+                    if (event !== 1) { // 1 means: total block!
+                        if (event === 0 || this.validateEventId(event)) {
+                            setValueToMatrix(userData, x, y, w, this.id);
+                            setValueToMatrix(userData, this.x, this.y, w, 0);
+                            this.x = x;
+                            this.y = y;
+                            if (event !== 0) {
+                                //call event
+                                for (var i = 0; i < this.eventListener.length; i++) {
+                                    var data = {};
+                                    var key = x + "_" + y;
+                                    if (key in this.grid.trapLookup) {
+                                        data = this.grid.trapLookup[key];
+                                    }
+                                    this.eventListener[i].call(this, event, data);
+                                }
+                            }
+                        }
+                    }
+                }
+            } else {
+                // BIG ELEMENT
+                var Y = y + this.h - 1;
+                var X = x + this.w - 1;
+                switch (dir) {
+                    case Directions.n:
+                        for(var nx = x;x < X; x++){
+
+                        }
+                        break;
+                    case Directions.s:
+
+                        break;
+                    case Directions.w:
+
+                        break;
+                    case Directions.e:
+
+                        break;
+                    case Directions.ne:
+
+                        break;
+                    case Directions.nw:
+
+                        break;
+                    case Directions.se:
+
+                        break;
+                    case Directions.sw:
+
+                        break;
+                }
+
+
+            }
+        }
+    };
+
+    GridEntity.prototype.forcePosition = function (x, y) {
+        if (this.w === 1 && this.h === 1) {
+            setValueToMatrix(this.grid.userData, this.x, this.y, this.grid.w, 0);
+            setValueToMatrix(this.grid.userData, this.x, this.y, this.grid.w, this.id);
+        } else {
             var w = this.grid.w;
             var X = this.x + this.w;
             var Y = this.y + this.h;
-            for(var x = this.x; x < X; x++){
-                for(var y = this.y; y < Y; y++){
-                    setValueToMatrix(this.grid.userData, x,y, w, 0);
+            for (var x = this.x; x < X; x++) {
+                for (var y = this.y; y < Y; y++) {
+                    setValueToMatrix(this.grid.userData, x, y, w, 0);
                 }
             }
             X = x + this.w;
             Y = y + this.h;
-            for(var x = this.x; x < X; x++){
-                for(var y = this.y; y < Y; y++){
-                    setValueToMatrix(this.grid.userData, x,y, w, this.id);
+            for (var x = this.x; x < X; x++) {
+                for (var y = this.y; y < Y; y++) {
+                    setValueToMatrix(this.grid.userData, x, y, w, this.id);
                 }
             }
         }
     };
 
 
-
-    function getValueFromMatrix(matrix, x, y, matrixW){
+    function getValueFromMatrix(matrix, x, y, matrixW) {
         return matrix[y * matrixW + x];
     };
 
-    function setValueToMatrix(matrix, x, y, matrixW, value){
+    function setValueToMatrix(matrix, x, y, matrixW, value) {
         matrix[y * matrixW + x] = value;
     }
 
 
-
     // ~~~~~~~~~~~~~~~~~~~~~~~~
     var root = this;
-    if(typeof module !== 'undefined' && module.exports){
+    if (typeof module !== 'undefined' && module.exports) {
         module.exports = OG;
-        if(root) root.OG = OG;
-    }else{
+        if (root) root.OG = OG;
+    } else {
         if (root) root.OG = OG;
         window.OG = OG;
     }
